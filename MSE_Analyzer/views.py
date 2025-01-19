38e29efeab209aed4f6b569a449f014c3bd2b367
MarkoMinovski <minovskimarco@gmail.com>
2025-01-19 01:34:04 +0100

    Added serverside rendering

--- MSE_Analyzer/views.py
+++ MSE_Analyzer/views.py
@@ -21,7 +21,22 @@ def contact(request):
 
 
 def services(request):
-    return render(request, 'MSE_Analyzer/services_intro.html')
+
+    api_resp_for_all_tickers = req_lib.get("http://127.0.0.1:5000/all")
+
+    json_all_tickers = api_resp_for_all_tickers.json()
+
+    for elem in json_all_tickers:
+        print(elem)
+        print('----')
+
+    context = {
+        'options_list': json_all_tickers
+    }
+
+    template = loader.get_template("MSE_Analyzer/services_intro.html")
+
+    return HttpResponse(template.render(context, request))
 
 
 @csrf_exempt
@@ -29,7 +44,6 @@ def analysed(request):
     if request.method == "POST":
         selected_ticker = request.POST.get('ticker')
         selected_interval = request.POST.get('interval')
-        interval_start = datetime.MINYEAR
 
         latest_available_date_resp = req_lib.get('http://127.0.0.1:5000/tickers/latest/str')
 
@@ -40,21 +54,48 @@ def analysed(request):
         # Return format: MM/DD/YYYY
         lds_parts = lds_str.split('/')
 
-        lds_datetime = datetime.datetime(int(lds_parts[2]), int(lds_parts[0]), int(lds_parts[1]))
+        interval_end = datetime.datetime(int(lds_parts[2]), int(lds_parts[0]), int(lds_parts[1]))
 
         if selected_interval == 'last-week':
-            interval_start = lds_datetime - timedelta(days=7)
+            interval_start = interval_end - timedelta(days=7)
         elif selected_interval == 'last-month':
-            interval_start = lds_datetime - timedelta(days=30)
+            interval_start = interval_end - timedelta(days=30)
         else:
-            interval_start = lds_datetime - timedelta(days=364)
+            interval_start = interval_end - timedelta(days=364)
+
+        interval_start_str = interval_start.strftime("%m/%d/%Y")
+        interval_end_str = interval_end.strftime("%m/%d/%Y")
+
+        interval_start_with_periods = interval_start_str.replace('/', '.')
+        interval_end_with_periods = interval_end_str.replace('/', '.')
+
+        url_endpoint = ("http://127.0.0.1:5000/tickers/analyze/oscillators/" + interval_start_with_periods + '/' +
+                        interval_end_with_periods + '/' + selected_ticker)
+
+        api_resp_for_oscillator_analysis = req_lib.get(url_endpoint)
+
+        json_array_from_oscillator_analysis_response = api_resp_for_oscillator_analysis.json()
+
+        json_array_purged = []
+
+        for dict_elem in json_array_from_oscillator_analysis_response:
+            current_dict_elem_replacement = {}
+            for key, value in dict_elem.items():
+                if "momentum" in key or "date_str" in key:
+                    current_dict_elem_replacement[key] = value
+            json_array_purged.append(current_dict_elem_replacement)
 
         template = loader.get_template('MSE_Analyzer/services_graph.html')
 
+        for elem in json_array_purged:
+            print(elem)
+            print("-----")
+
         context = {
             'interval_start': interval_start.strftime("%m/%d/%Y"),
-            'interval_end': lds_datetime.strftime("%m/%d/%Y"),
+            'interval_end': interval_end.strftime("%m/%d/%Y"),
             'ticker_code': selected_ticker,
+            'json_array_momentum_oscillators': json_array_purged
         }
 
         return HttpResponse(template.render(context, request))
